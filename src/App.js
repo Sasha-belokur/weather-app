1a8a5a1fa98a24d80dfa6657c0beaf6ba724fd94
Alex Belokur <alex.belokur17@gmail.com>
2018-04-18 21:47:07 +0300

    added method for download forecast data

--- src/App.js
+++ src/App.js
@@ -1,14 +1,52 @@
 import React, { Component } from "react";
+import axios from "axios";
 
 import Header from "./components/Header";
 import Forecast from "./components/Forecast";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      isLoaderShown: true
+    };
+
+    this.getForecastData = this.getForecastData.bind(this);
+  }
+
+  getForecastData(city) {
+    if (!city) return;
+
+    const link = "http://api.openweathermap.org/data/2.5/forecast";
+    const appid = "889a099ef04f7508e2286e417f9837d0";
+    axios
+      .get(link, {
+        params: {
+          q: city,
+          units: "metric",
+          appid
+        }
+      })
+      .then(res => {
+        this.setState({
+          city: res.data.city.name,
+          forecast: res.data,
+          isLoaderShown: false
+          // weather: getForecast(res.data)
+        });
+      })
+      .catch(error => console.dir(error));
+  }
+
+  componentDidMount() {
+    this.getForecastData("dnipro");
+  }
+
   render() {
     return (
       <div>
         <Header />
-        <Forecast />
+        <Forecast isLoaderShown={this.state.isLoaderShown} />
       </div>
     );
   }
