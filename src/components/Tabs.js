d27fc353bc01e47224018bd13bea2587ed207a8b
Alex Belokur <alex.belokur17@gmail.com>
2018-04-19 11:46:38 +0300

    created function handleTabAction, add access to tabs via keyboard

--- src/components/Tabs.js
+++ src/components/Tabs.js
@@ -4,37 +4,55 @@ class Tabs extends Component {
   constructor() {
     super();
     this.tabClickHandler = this.tabClickHandler.bind(this);
+    this.tabKeyUpHandler = this.tabKeyUpHandler.bind(this);
+    this.handleTabAction = this.handleTabAction.bind(this);
   }
 
-  tabClickHandler(e) {
-    const tab = e.target;
-    const chartType = tab.dataset.chartType;
+  handleTabAction(tab, chartType) {
     const activeTab = document.querySelector(".tabs__item--active");
-
-    if (!chartType) return;
     activeTab.classList.remove("tabs__item--active");
     tab.classList.add("tabs__item--active");
 
     this.props.onTabClick(chartType);
   }
 
+  tabClickHandler(e) {
+    const tab = e.target;
+    const chartType = tab.dataset.chartType;
+
+    if (!chartType) return;
+    this.handleTabAction(tab, chartType);
+  }
+
+  tabKeyUpHandler(e) {
+    const tab = e.target;
+    const chartType = tab.dataset.chartType;
+
+    if (!chartType || e.keyCode !== 13) return;
+    this.handleTabAction(tab, chartType);
+  }
+
   render() {
     return (
-      <ul className="tabs" onClick={this.tabClickHandler}>
-        <li tabIndex="1" data-chart-type="wind" className="tabs__item">
+      <ul
+        className="tabs"
+        onClick={this.tabClickHandler}
+        onKeyUp={this.tabKeyUpHandler}
+      >
+        <li tabIndex="0" data-chart-type="wind" className="tabs__item">
           Wind
         </li>
         <li
-          tabIndex="1"
+          tabIndex="0"
           data-chart-type="temp"
           className="tabs__item tabs__item--active"
         >
           Temperature
         </li>
-        <li tabIndex="1" data-chart-type="presure" className="tabs__item">
+        <li tabIndex="0" data-chart-type="presure" className="tabs__item">
           Presure
         </li>
-        <li tabIndex="1" data-chart-type="humidity" className="tabs__item">
+        <li tabIndex="0" data-chart-type="humidity" className="tabs__item">
           Humidity
         </li>
       </ul>
